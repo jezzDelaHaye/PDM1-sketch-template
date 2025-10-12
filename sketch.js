920f81cf6f4bd6d15aab6f80d728e5c1d7a38bfe
Jeremy Southern <jezzd11@outlook.com>
2025-10-12 17:51:26 +0100

    Added movement features

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 const GRID_SIZE = 20;
 const CANVAS_SIZE = 600;
 const OFFSET = GRID_SIZE /2;
+let gameOver = false;
 let headX = 300;
 let headY = 300;
+let xDir = 1;
+let yDir = 0;
 let foodX;
 let foodY;
 
@@ -11,24 +14,25 @@ function setup()
     createCanvas(CANVAS_SIZE,CANVAS_SIZE)
     background(0);
     createFood();
-    console.log(foodX,foodY)
     frameRate(3);
     
 }
 function draw()
 {
-    square(headX += GRID_SIZE,headY,GRID_SIZE);
+    if (!gameOver)
+    {
+        square(headX += GRID_SIZE * xDir,headY += GRID_SIZE * yDir,GRID_SIZE);
+    }
     circle(foodX, foodY, GRID_SIZE);
     boundaryCheck(headX,headY);   
+    console.log(foodX,foodY)
 
 }
-
 function boundaryCheck(x,y)
 {
     if (x >= CANVAS_SIZE|| x <=0 || y >= CANVAS_SIZE || y<=0 )
     {
-        background(0);
-        console.log("game over !")
+        (gameOver === true);
     }
 
 }
@@ -36,4 +40,38 @@ function createFood()
 {
     foodX = (round (random(0,CANVAS_SIZE)/GRID_SIZE) * GRID_SIZE + OFFSET)
     foodY = (round (random(0,CANVAS_SIZE)/GRID_SIZE) * GRID_SIZE + OFFSET)
+}
+function keyPressed()
+{
+    
+    if (key === 'w' || keyCode === UP_ARROW)
+    {
+        //up
+        xDir = 0;
+        yDir = -1;
+        console.log("up")
+    }
+    if (key === 's' || keyCode === DOWN_ARROW)
+    {
+        //down
+        xDir = 0;
+        yDir = 1;
+        console.log("down")
+    }
+    if (key === 'a' || keyCode === LEFT_ARROW)
+    {
+        //left
+        xDir = -1;
+        yDir = 0;
+        console.log("left")
+    }
+    if (key === 'd' || keyCode === RIGHT_ARROW)
+    {
+        //right
+        xDir = 1;
+        yDir = 0;
+        console.log("right")
+    }
+    
+    
 }
