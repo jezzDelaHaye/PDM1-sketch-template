85cd5d5c4d6f63cbf693fab4a3bf9d91f0ebb8cf
Jeremy Southern <jezzd11@outlook.com>
2025-10-12 17:23:23 +0100

    Restarted - aiming to make code higher quality, shorter, more robust

--- sketch.js
+++ sketch.js
@@ -1,74 +1,39 @@
 const GRID_SIZE = 20;
+const CANVAS_SIZE = 600;
+const OFFSET = GRID_SIZE /2;
 let headX = 300;
 let headY = 300;
 let foodX;
 let foodY;
-let col = 255;
-let xDir = 1;
-let yDir = 0;
-let direction = 0;
+
 function setup()
 {
-    createCanvas(600,600);
+    createCanvas(CANVAS_SIZE,CANVAS_SIZE)
     background(0);
-    foodX = random(0,600);
-    foodY = random(0,600);
+    createFood();
+    console.log(foodX,foodY)
     frameRate(3);
+    
 }
 function draw()
 {
-    fill(255)
-    move();
-    square(headX,headY,GRID_SIZE);
-    circle(foodX,foodY,GRID_SIZE);
-    //checkBoundaries();
-
+    square(headX += GRID_SIZE,headY,GRID_SIZE);
+    circle(foodX, foodY, GRID_SIZE);
+    boundaryCheck(headX,headY);   
 
 }
-function keyPressed()
-{
-    if (key === 'w')
-    {
-        direction = 1;
-    }
-    if (key === 's')
-    {
-        direction = 2;
-    }
-    if (key === 'a')
-    {
-        direction = 3;
-    }
-    if (key === 'd')
-    {
-        direction = 4; 
-    }
-}
-function checkBoundaries()
-{
-    if (foodX > width || foodX < 0 || foodY > height || foodY < 0)
-    {
-        col = 0;
-        console.log(gameOver)
-    }
-}
-function move()
-{
-    if (direction === 1)
-    {
-        headY=-GRID_SIZE;
-    }
-    if (direction === 2)
-    {
-        headY+-GRID_SIZE;
-    }
-    if(direction ===3)
-    {
-        headX =-GRID_SIZE;
-    }
-    if (direction === 4)
-    {
-        headX =+ GRID_SIZE;
-    }
-}
 
+function boundaryCheck(x,y)
+{
+    if (x >= CANVAS_SIZE|| x <=0 || y >= CANVAS_SIZE || y<=0 )
+    {
+        background(0);
+        console.log("game over !")
+    }
+
+}
+function createFood()
+{
+    foodX = (round (random(0,CANVAS_SIZE)/GRID_SIZE) * GRID_SIZE + OFFSET)
+    foodY = (round (random(0,CANVAS_SIZE)/GRID_SIZE) * GRID_SIZE + OFFSET)
+}
